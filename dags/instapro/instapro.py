133c07cdd9cd3c5637673632a6e36cc97749188c
Kohinoor Biswas <kohinoorbiswas@Kohinoors-MacBook-Air.local>
2023-07-02 07:41:21 +0530

    call helm charts

--- dags/instapro/instapro.py
+++ dags/instapro/instapro.py
@@ -1,45 +1,34 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "owner": "Kohinoor Biswas",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+}
 
 with DAG(
     dag_id="instapro",
-    default_args={
-        "owner": "Kohinoor Biswas",
-        "depends_on_past": False,
-        "email_on_failure": False,
-        "email_on_retry": False,
-    },
+    default_args=default_args,
     start_date=datetime(2021, 1, 1, 0, 0),
     schedule_interval="@daily",
     catchup=False,
 ) as dag:
-    # Task 1: Execute Helm Chart 1
-    task1 = KubernetesPodOperator(
-        task_id='execute_helm_chart1',
-        namespace='airflow',  # Specify the namespace as "airflow"
-        image='helm:latest',
-        cmds=['helm', 'install', 'instapro-data-loader', '/Users/kohinoorbiswas/instapro/charts/instapro-data-loader/instapro-data-loader-0.1.0.tgz'],
-        dag=dag,
-    )
+    helm_charts = [
+        "instapro-data-loader",
+        "instapro-data-modeller",
+        "instapro-data-transformer",
+    ]
 
-    # Task 2: Execute Helm Chart 2
-    task2 = KubernetesPodOperator(
-        task_id='execute_helm_chart2',
-        namespace='airflow',  # Specify the namespace as "airflow"
-        image='helm:latest',
-        cmds=['helm', 'install', 'instapro-data-modeller', '/Users/kohinoorbiswas/instapro/charts/instapro-data-modeller/instapro-data-modeller-0.1.0.tgz'],
-        dag=dag,
-    )
-
-    # Task 3: Execute Helm Chart 3
-    task3 = KubernetesPodOperator(
-        task_id='execute_helm_chart3',
-        namespace='airflow',  # Specify the namespace as "airflow"
-        image='helm:latest',
-        cmds=['helm', 'install', 'instapro-data-tranformer', '/Users/kohinoorbiswas/instapro/charts/instapro-data-transformer/instapro-data-transformer-0.1.0.tgz'],
-        dag=dag,
-    )
-
-    # Define the task dependencies
-    task1 >> task2 >> task3
+    for i, chart in enumerate(helm_charts, 1):
+        command = f"helm install {chart} /Users/kohinoorbiswas/instapro/charts/{chart}/{chart}-0.1.0.tgz"
+        task = BashOperator(
+            task_id=f"execute_helm_chart{i}",
+            bash_command=command,
+            dag=dag,
+        )
+        if i > 1:
+            task.set_upstream(prev_task)
+        prev_task = task
