3ca041e235574b4d8054428e410e4a4f03834768
Kohinoor Biswas <kohinoorbiswas@Kohinoors-MacBook-Air.local>
2023-07-02 07:49:08 +0530

    call helm charts

--- dags/instapro/instapro.py
+++ dags/instapro/instapro.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import logging
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 
@@ -8,6 +9,10 @@ default_args = {
     "email_on_failure": False,
     "email_on_retry": False,
 }
+
+def log_operator_result(task_instance, **kwargs):
+    logger = logging.getLogger(__name__)
+    logger.info("Task '%s' finished with result: %s", task_instance.task_id, task_instance.xcom_pull(task_ids=task_instance.task_id))
 
 with DAG(
     dag_id="instapro",
@@ -29,6 +34,11 @@ with DAG(
             bash_command=command,
             dag=dag,
         )
+
+        # Add the logging callback to the task
+        task.on_success_callback = log_operator_result
+        task.on_failure_callback = log_operator_result
+
         if i > 1:
             task.set_upstream(prev_task)
         prev_task = task
